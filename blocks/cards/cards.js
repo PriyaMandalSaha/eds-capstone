ae4e66d0023be812e33e45eeaeb6e53ed32af00c
Priya <155519579+PriyaMandalSaha@users.noreply.github.com>
2024-08-23 17:00:31 +0530

    update card

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -17,8 +17,10 @@ export default function decorate(block) {
   });
 
   ul.querySelectorAll('picture > img').forEach((img) =>
-    img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false,
-    [{ width: '750' }]))
+    img.closest('picture').replaceWith(createOptimizedPicture(
+    img.src, img.alt, false,
+      [{ width: '750' }]
+    ))
   );
 
   ul.querySelectorAll('.cards-card-body .button-container a').forEach((a, index) => {
