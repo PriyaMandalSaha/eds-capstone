220b0d852e6d88fbe4392579e101a2863dfa0ea0
Priya <155519579+PriyaMandalSaha@users.noreply.github.com>
2024-08-23 22:07:07 +0530

    update social link

--- blocks/social-link/social-link.js
+++ blocks/social-link/social-link.js
@@ -1,27 +1,18 @@
 export default function decorate(block) {
-  // Select the div with the class 'medialinks'
   const mediaLinksDiv = block.querySelector('p');
 
-  // If the div exists, proceed with adding class names and other operations
   if (mediaLinksDiv) {
+    const aTags = mediaLinksDiv.querySelectorAll('a');
 
-      // Select all <a> tags inside the mediaLinksDiv
-      const aTags = mediaLinksDiv.querySelectorAll('a');
+    aTags.forEach((aTag) => {
+      const title = aTag.getAttribute('title');
 
-      // Iterate over each <a> tag
-      aTags.forEach((aTag) => {
-          // Get the title attribute from the <a> tag
-          const title = aTag.getAttribute('title');
-          // If the title exists, add a class to the <a> tag using the title
-          if (title) {
-              // Replace spaces or any special characters in the title for valid class names
-              const className = `${title.toLowerCase().replace(/\s+/g, '-')}-link`;
-              aTag.classList.add(className);
-          }
-      });
+      if (title) {
+        const className = `${title.toLowerCase().replace(/\s+/g, '-')}-link`;
+        aTag.classList.add(className);
+      }
+    });
 
-      // Finally, append the modified mediaLinksDiv back to the block
-      block.append(mediaLinksDiv);
+    block.append(mediaLinksDiv);
   }
 }
-
